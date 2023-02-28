698f0d17961640fafcc234b6b3087ec1c3c17656
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-02-28 00:36:47 -0500

    project requirements met

---
+++ IAmChatGPT/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  IAmChatGPT
+//
+//  Created by Daize Njounkeng on 2/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var GPTQuote: UILabel!
+    @IBOutlet weak var GPTName: UITextField!
+    @IBOutlet weak var GPTImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GPTName.layer.opacity = 0.5
+    }
+    //    @IBAction func firstButtonClicked(_ sender: UIButton) {
+    //        print("I was clicked")
+    //        performSegue(withIdentifier: "oneSeague", sender: sender)
+    //
+    //        if let tappedView = sender.view {
+    //            performSegue(withIdentifier: "oneSegue", sender: tappedView)
+    //        }
+    //    }
+    
+    @IBAction func didFirstTap(_ sender: UITapGestureRecognizer) {
+        print("I was clicked")
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "oneSeague", sender: tappedView)
+        }
+    }
+
+    @IBAction func didSecondTap(_ sender: UITapGestureRecognizer) {
+        print("Second was clicked")
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "twoSeague", sender: tappedView)
+        }
+    }
+    
+    
+    @IBAction func didThirdTap(_ sender: UITapGestureRecognizer) {
+        print("Third was clicked")
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "threeSeague", sender: tappedView)
+        }
+    }
+}
---
+++ IAmChatGPT/TwoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TwoViewController.swift
+//  IAmChatGPT
+//
+//  Created by Daize Njounkeng on 2/27/23.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
